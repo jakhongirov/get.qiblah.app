6cff981b6eb2ae92be2de51cf11059b5cf2fca00
jakhongirov <johongirovdiyorbek08@gmail.com>
2024-06-20 09:19:40 +0500

    add

--- src/redirect/redirect.jsx
+++ src/redirect/redirect.jsx
@@ -11,12 +11,26 @@ function Redirect() {
 		}
 		// Check if the user is using an Android device
 		else if (/Android/i.test(navigator.userAgent)) {
-			window.location.href = `https://play.google.com/store/apps/details?id=uz.behad.qiblah.namoz&pcampaignid=${campaign_id ? campaign_id : "noparam"}`; // Replace with your Google Play link and package name
+			if (
+				campaign_id == 'times' ||
+				campaign_id == 'quran' ||
+				campaign_id == 'names' ||
+				campaign_id == 'adhkar' ||
+				campaign_id == 'app'
+			) {
+				window.location.href = `https://play.google.com/store/apps/details?id=uz.behad.qiblah.namoz&pcampaignid=share_${campaign_id}`;
+			} else {
+				window.location.href = `https://play.google.com/store/apps/details?id=uz.behad.qiblah.namoz&pcampaignid=${
+					campaign_id ? campaign_id : 'noparam'
+				}`;
+			}
 		}
 		// If the device is not iOS or Android, you can handle it differently or redirect to a generic page
 		else {
 			// Redirect to a generic page
-			window.location.href = `https://play.google.com/store/apps/details?id=uz.behad.qiblah.namoz&pcampaignid=${campaign_id ? campaign_id : "noparam"}`;
+			window.location.href = `https://play.google.com/store/apps/details?id=uz.behad.qiblah.namoz&pcampaignid=${
+				campaign_id ? campaign_id : 'noparam'
+			}`;
 		}
 	}, [campaign_id]);
 
